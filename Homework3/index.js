260c4224942fdb742d66e88e6dc4ea4d381cba08
Peter Ivashko <petroivashko2803@gmail.com>
2023-01-28 21:44:05 +0200

    Homework3

---
+++ Homework3/index.js
@@ -0,0 +1,9 @@
+let userStr1 = prompt("First string:");
+let userStr2 = prompt("Second string:");
+let userStr3 = prompt("Third string:");
+console.log('Рядки від користувача - ' + (userStr1 + userStr2 + userStr3));
+
+let num = 12345;
+let str = String(num);
+let output = str.split('');
+console.log(output)
