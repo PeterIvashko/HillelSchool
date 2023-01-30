b584f8b68414a9ba7fb43fceb36a7c0019cc9ed9
Peter Ivashko <petroivashko2803@gmail.com>
2023-01-30 20:57:52 +0200

    Homework 4

--- Homework3/index.js
+++ Homework3/index.js
@@ -3,7 +3,7 @@ let userStr2 = prompt("Second string:");
 let userStr3 = prompt("Third string:");
 console.log('Рядки від користувача - ' + (userStr1 + userStr2 + userStr3));
 
-let num = 12345;
-let str = String(num);
-let output = str.split('');
-console.log(output)
+let number = 12345;
+let str = String(number);
+let output = str[0] + ' ' + str[1] + ' ' + str[2] + ' ' + str[3] + ' ' + str[4]
+console.log(output);
