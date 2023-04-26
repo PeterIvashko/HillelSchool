4612bdc42aa12c1d5de08ea1b2edfc929dda4ae5
Peter Ivashko <petroivashko2803@gmail.com>
2023-04-26 20:50:44 +0300

    added homework 13

--- Homework12/script.js
+++ Homework12/script.js
@@ -1,8 +1,12 @@
-const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg"];
+let images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg'];
 
-const randomIndex = Math.floor(Math.random() * images.length);
+// Випадковим чином вибрати одне зображення
+let randomIndex = Math.floor(Math.random() * images.length);
+let randomImage = images[randomIndex];
 
-const img = document.createElement("img");
-img.src = `images/${images[randomIndex]}`;
+// Створити HTML-елемент img та встановити йому властивість src
+let img = document.createElement('img');
+img.src = 'images/' + randomImage;
 
-document.getElementById("image-container").appendChild(img);
+// Додати створений HTML-елемент до документу
+document.body.appendChild(img);
--- Homework13/script.js
+++ Homework13/script.js
@@ -1,12 +1,13 @@
-let images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg'];
+"use strict";
 
-// Випадковим чином вибрати одне зображення
-let randomIndex = Math.floor(Math.random() * images.length);
-let randomImage = images[randomIndex];
-
-// Створити HTML-елемент img та встановити йому властивість src
-let img = document.createElement('img');
-img.src = 'images/' + randomImage;
-
-// Додати створений HTML-елемент до документу
-document.body.appendChild(img);
+function redirectToOtherSite(url) {
+  if (typeof url !== "string" || !url.trim()) {
+    return;
+  }
+  if (!url.startsWith("http://") && !url.startsWith("https://")) {
+    url = "http://" + url;
+  }
+  const newWindow = window.open();
+  newWindow.opener = null;
+  newWindow.location.href = url;
+}
