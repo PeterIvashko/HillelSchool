3ef989e167656ab635232258c7ab3a5116962c2d
Peter Ivashko <petroivashko2803@gmail.com>
2023-04-29 10:32:29 +0300

    bag fixed

--- Homework16/script.js
+++ Homework16/script.js
@@ -1,22 +1,48 @@
 let currentImageIndex = 0;
 const images = ["1.jpg", "2.jpg", "3.jpg"];
 
-function nextImage() {
-  currentImageIndex++;
-  if (currentImageIndex >= images.length) {
-    currentImageIndex = images.length - 1;
-    const nextButton = document.getElementById('nextButton');
-    if (nextButton) {
-      nextButton.style.display = 'none';
-    }
-  }
+function showCurrentImage() {
   const image = document.getElementById('image');
   if (image) {
     image.src = images[currentImageIndex];
   }
+
+  const prevButton = document.getElementById('prevButton');
+  if (prevButton) {
+    if (currentImageIndex === 0) {
+      prevButton.style.display = 'none';
+    } else {
+      prevButton.style.display = 'block';
+    }
+  }
+
+  const nextButton = document.getElementById('nextButton');
+  if (nextButton) {
+    if (currentImageIndex === images.length - 1) {
+      nextButton.style.display = 'none';
+    } else {
+      nextButton.style.display = 'block';
+    }
+  }
+}
+
+function nextImage() {
+  currentImageIndex++;
+  if (currentImageIndex >= images.length) {
+    currentImageIndex = images.length - 1;
+  }
+  showCurrentImage();
+  
   const prevButton = document.getElementById('prevButton');
   if (prevButton) {
     prevButton.style.display = 'block';
+  }
+  
+  const nextButton = document.getElementById('nextButton');
+  if (nextButton) {
+    if (currentImageIndex === images.length - 1) {
+      nextButton.style.display = 'none';
+    }
   }
 }
 
@@ -24,17 +50,20 @@ function prevImage() {
   currentImageIndex--;
   if (currentImageIndex < 0) {
     currentImageIndex = 0;
-    const prevButton = document.getElementById('prevButton');
-    if (prevButton) {
-      prevButton.style.display = 'none';
-    }
   }
-  const image = document.getElementById('image');
-  if (image) {
-    image.src = images[currentImageIndex];
-  }
+  showCurrentImage();
+  
   const nextButton = document.getElementById('nextButton');
   if (nextButton) {
     nextButton.style.display = 'block';
   }
+  
+  const prevButton = document.getElementById('prevButton');
+  if (prevButton) {
+    if (currentImageIndex === 0) {
+      prevButton.style.display = 'none';
+    }
+  }
 }
+
+showCurrentImage();
