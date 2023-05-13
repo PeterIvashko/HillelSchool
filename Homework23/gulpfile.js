ee3b20e6b8a5aa79baf7f0597951efc768b924a2
Peter Ivashko <petroivashko2803@gmail.com>
2023-05-13 11:51:32 +0300

    homework 23

--- Homework23/gulpfile.js
+++ Homework23/gulpfile.js
@@ -1,20 +1,46 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const less = require('gulp-less');
 const concat = require('gulp-concat');
-const minifyCss = require('gulp-minify-css');
 const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const del = require('del');
 
-gulp.task('minify-css', () => {
-  return gulp.src('src/**/*.css')
-    .pipe(concat('styles.min.css'))
-    .pipe(minifyCss())
-    .pipe(gulp.dest('dist/css'));
+// Компіляція Sass в CSS
+gulp.task('sass', function() {
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./src/css'));
 });
 
-gulp.task('minify-js', () => {
-  return gulp.src('src/**/*.js')
-    .pipe(concat('scripts.min.js'))
+// Компіляція Less в CSS
+gulp.task('less', function() {
+  return gulp.src('./src/less/**/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('./src/css'));
+});
+
+// Конкатенація та мінімізація JS коду
+gulp.task('scripts', function() {
+  return gulp.src('./src/js/**/*.js')
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(uglify())
-    .pipe(gulp.dest('dist/js'));
+    .pipe(gulp.dest('./dist/js'));
 });
 
-gulp.task('default', gulp.parallel('minify-css', 'minify-js'));
+// Очистка папки з готовим проектом
+gulp.task('clean', function() {
+  return del(['./dist/**/*']);
+});
+
+// Спостереження за змінами в файлах
+gulp.task('watch', function() {
+  gulp.watch('./src/scss/**/*.scss', gulp.series('sass'));
+  gulp.watch('./src/less/**/*.less', gulp.series('less'));
+  gulp.watch('./src/js/**/*.js', gulp.series('scripts'));
+});
+
+// Запуск режиму розробки
+gulp.task('default', gulp.series('clean', gulp.parallel('sass', 'less', 'scripts'), 'watch'));
